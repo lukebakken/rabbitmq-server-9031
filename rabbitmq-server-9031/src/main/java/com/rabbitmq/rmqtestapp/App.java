67c1f45dfefb63748bd7366502fc670a9c1aba3a
Luke Bakken <luke@bakken.io>
2023-08-09 07:26:45 -0700

    Initial code commit

---
+++ rabbitmq-server-9031/src/main/java/com/rabbitmq/rmqtestapp/App.java
@@ -0,0 +1,13 @@
+package com.rabbitmq.rmqtestapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ rabbitmq-server-9031/src/test/java/com/rabbitmq/rmqtestapp/AppTest.java
@@ -0,0 +1,20 @@
+package com.rabbitmq.rmqtestapp;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
