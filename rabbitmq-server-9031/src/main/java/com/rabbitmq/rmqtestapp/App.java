ac6a72c0ade931788c99404d69dcdca9c324fb52
Luke Bakken <luke@bakken.io>
2023-08-09 07:33:37 -0700

    Application compiles

--- rabbitmq-server-9031/src/main/java/com/rabbitmq/rmqtestapp/App.java
+++ rabbitmq-server-9031/src/main/java/com/rabbitmq/rmqtestapp/App.java
@@ -1,13 +1,24 @@
 package com.rabbitmq.rmqtestapp;
 
-/**
- * Hello world!
- *
- */
+import jakarta.jms.JMSException;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println("[INFO] rabbitmq-server-9031 test application");
+        org.apache.qpid.jms.JmsConnectionFactory jmsConnectionFactory = new org.apache.qpid.jms.JmsConnectionFactory("amqps://host:5671");
+        jmsConnectionFactory.setUsername("BOB");
+        jmsConnectionFactory.setPassword("PASSWORD");
+        try
+        {
+            jmsConnectionFactory.createConnection().createSession();
+        }
+        catch (JMSException ex)
+        {
+            System.err.println("[ERROR] createSession() failed:");
+            ex.printStackTrace();
+            System.err.println(ex);
+        }
     }
 }
